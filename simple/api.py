ce0d5367571172560a80e5bfb544b79416c67ec3
Feldwor <BoQsc@users.noreply.github.com>
2025-05-25 19:21:51 +0300

    update

--- simple/api.py
+++ simple/api.py
@@ -7,6 +7,17 @@ async def handler(request):
 app = web.Application()
 app.router.add_get('/', handler)
 
+@web.middleware 
+async def cors_handler(request, handler):
+    response = await handler(request)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+    
+app.middlewares.append(cors_handler)
+    
+
 ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ctx.load_cert_chain('cert.pem', 'key.pem')
 web.run_app(app, ssl_context=ctx, port=443)
